d1ea94625e0d5f6f5765ebc93b2438eb5244c53d
auliahrs <hasnhiahaulia27@gmail.com>
2024-10-30 03:32:15 +0800

    gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:wellzy/splash/splash_page.dart';
 import 'package:wellzy/Views/auth/login_page.dart';
 import 'package:wellzy/Views/auth/signup_page.dart';
 import 'package:wellzy/Views/homepage/homepage.dart';
 import 'package:wellzy/Views/auth/forgot_password_page.dart';
 
-import 'Views/homepage/homepage.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  runApp(WellzyApp());
+  try {
+    await Firebase.initializeApp();
+    runApp(WellzyApp());
+  } catch (e) {
+    runApp(ErrorApp(error: e.toString()));
+  }
 }
 
 class WellzyApp extends StatelessWidget {
@@ -27,6 +33,29 @@ class WellzyApp extends StatelessWidget {
         '/home': (context) => const HomePage(),
         '/forgot_password': (context) => const ForgotPasswordPage(),
       },
+    );
+  }
+}
+
+// ErrorApp widget to display a Firebase initialization error message
+class ErrorApp extends StatelessWidget {
+  final String error;
+
+  ErrorApp({required this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Initialization Error')),
+        body: Center(
+          child: Text(
+            'Failed to initialize Firebase: $error',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.red, fontSize: 16),
+          ),
+        ),
+      ),
     );
   }
 }
